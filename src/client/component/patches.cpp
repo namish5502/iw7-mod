2b3ddb55d150f94a1ecdbb72d826a7f0b7649acf
namish1221 <129816822+namish5502@users.noreply.github.com>
2025-07-01 11:33:08 -0400

    Update patches.cpp
    Desabilitado de contratos para el idioma latino

--- src/client/component/patches.cpp
+++ src/client/component/patches.cpp
@@ -300,7 +300,6 @@ namespace patches
 			utils::hook::set<uint8_t>(0x1405C90C0, 0xC3);
 
 			// killswitches
-			dvars::override::register_bool("mission_team_contracts_enabled", true, game::DVAR_FLAG_READ);
 			dvars::override::register_bool("killswitch_store", false, game::DVAR_FLAG_READ);
 			dvars::override::register_bool("killswitch_quartermaster", false, game::DVAR_FLAG_READ);
 			dvars::override::register_bool("killswitch_cod_points", false, game::DVAR_FLAG_READ);
